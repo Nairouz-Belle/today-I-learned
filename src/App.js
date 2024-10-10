b998c9b771f033393b9fd36f6a75885088d07707
Nairouz-Belle <nairouz.belledjehem@gmail.com>
2024-10-10 20:05:09 +0100

    Uploading fact to SUPABASE.

--- src/App.js
+++ src/App.js
@@ -2,39 +2,39 @@ import { useEffect, useState } from 'react';
 import './style.css'
 import supabase from './supabase';
 
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
+// const initialFacts = [
+//   {
+//     id: 1,
+//     text: "React is being developed by Meta (formerly facebook)",
+//     source: "https://opensource.fb.com/",
+//     category: "technology",
+//     votesInteresting: 24,
+//     votesMindblowing: 9,
+//     votesFalse: 4,
+//     createdIn: 2021,
+//   },
+//   {
+//     id: 2,
+//     text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+//     source:
+//       "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+//     category: "society",
+//     votesInteresting: 11,
+//     votesMindblowing: 2,
+//     votesFalse: 0,
+//     createdIn: 2019,
+//   },
+//   {
+//     id: 3,
+//     text: "Lisbon is the capital of Portugal",
+//     source: "https://en.wikipedia.org/wiki/Lisbon",
+//     category: "society",
+//     votesInteresting: 8,
+//     votesMindblowing: 3,
+//     votesFalse: 1,
+//     createdIn: 2015,
+//   },
+// ];
 
 /*function Counter(){
   //arg1: count. arg2: function named setCount 
@@ -50,16 +50,21 @@ function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts,setFacts] = useState([]);
   const [isLoading,setIsLoading] = useState(false);
-  
-
+  const [currentCategory,setCurrentCategory] = useState("all");
+//Use effect only works one time when the page apload. so add currentCategoy to the array of useEffect (the second parameter) so whenever the the currentCategory state changes the useEffect will apply.
   useEffect(function () {
 
     async function getFact(){
       setIsLoading(true);
-      let{data: facts,error} = await supabase.from("facts")
-      .select("*")
-      .order("voteinteresting",{  ascending: false  });
-      console.log(error);
+    //Building the query
+      let query = supabase.from("facts").select("*");
+
+      if(currentCategory !== "all")
+        query = query.eq("category",currentCategory)
+      
+      const {data: facts,error} = await query.order("voteinteresting",{  ascending: false  })
+      .limit(1000);
+      //console.log(error);
       if(!error)
        setFacts(facts);
       else
@@ -68,7 +73,7 @@ function App() {
       setIsLoading(false);
     }
     getFact();
-  }, []);
+  }, [currentCategory]);
 
   return (
     <>
@@ -78,7 +83,7 @@ function App() {
       {showForm ? (<NewFactForm setFacts={setFacts} setShowForm = {setShowForm} />) : null}
 
       <main className='main'>
-        <CategoryFilter/>
+        <CategoryFilter setCurrentCategory = {setCurrentCategory} />
         {isLoading ? <Loader/> : <FactList facts={facts} />}
         
       </main>
@@ -128,28 +133,34 @@ function NewFactForm({setFacts, setShowForm}){
   const [text, setText] = useState("");
   const [source, setSource] = useState("http://example.com");
   const [category,setCategory] = useState("");
+  const [isUploading,setIsUploading] = useState(false);
   const textLength = text.length;
 
-  function handleSubmit(e){
+  async function handleSubmit(e){
     // 1. Prevent browser reload
     e.preventDefault();
-    console.log(text, source,category);
-
     //2. Check if the data is valid. If so create new fact
     if(text && isValidHttpUrl(source) && category && textLength <= 200) {
       //3. Create a new fact
-      const newFact = {
-        id: Math.round(Math.random * 100000000),
-        text: text,
-        source,
-        category,
-        votesInteresting: 0,
-        votesMindblowing:0,
-        votesFalse: 0,
-        createdIn: new Date().getFullYear(),
-      };
+      //  const newFact = {
+      //   id: Math.round(Math.random * 100000000),
+      //    text: text,
+      //    source,
+      //    category,
+      //    votesInteresting: 0,
+      //    votesMindblowing:0,
+      //    votesFalse: 0,
+      //    createdIn: new Date().getFullYear(),
+      //  };
+      //3. Upload the new fact to supabase and receive the new fact object
+      setIsUploading(true);
+       const { data: newFact,error } = await supabase.from("facts").insert([{text,source,category}]).select();
+       setIsUploading(false);
+       console.log(error);
+       console.log(newFact);
+
      //4. Add the fact to the UI: add the fact to state 
-     setFacts((facts) => [newFact,...facts]);
+     setFacts((facts) => [newFact[0],...facts]);
      //5. Reset input fields
      setText('');
      setSource('');
@@ -171,30 +182,36 @@ function NewFactForm({setFacts, setShowForm}){
         <input type="text" placeholder="Trustworthy source..."
         value={source}
         onChange={(e)=> setSource(e.target.value)}
+        disabled={isUploading}
         />
-        <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+        <select value={category} onChange={(e)=>setCategory(e.target.value)}
+        disabled={isUploading}  
+        >
           <option value="">Choose Category</option>
           {
             CATEGORIES.map((cat)=> (<option key={cat.name} value={cat.name}>{cat.name.toUpperCase()}</option>))
           }
         </select>
-        <button className="btn btn-large">Post</button>
+        <button className="btn btn-large" disabled={isUploading}>Post</button>
   </form>;
 }
 
 
 
-function CategoryFilter(){
+function CategoryFilter({setCurrentCategory}){ // props "setCurrentCategory" is to add that function (the props) to the categories.
   return (
     <aside>
       <ul>
         <li className="category">
-              <button className="btn btn-all-categories">All</button>
+              <button className="btn btn-all-categories" onClick={() => {setCurrentCategory("all")}}>All</button>
         </li>
 
         {CATEGORIES.map((cat)=> 
-           <li key={cat.name} className="category">
-           <button className="btn btn-category" style={{backgroundColor:cat.color}} >{cat.name}</button>
+           <li key={cat.name} 
+           className="category">
+           <button  className="btn btn-category" style={{backgroundColor:cat.color}} 
+           onClick={() => {setCurrentCategory(cat.name)}}
+           >{cat.name}</button>
    </li>
         )}
         
@@ -204,8 +221,8 @@ function CategoryFilter(){
 }
 
 function FactList({facts}){
-  //temporary (while we're using the up fake data.)
-  
+  if(facts.length === 0)
+    return <p className='message'>No facts for this category yet! Create the first one ✌</p>
 
   return (<section>
     <ul className="facts-list">
@@ -229,9 +246,9 @@ function Fact({fact}){
               </p> 
               <span className="tag" style={{backgroundColor:CATEGORIES.find((cat)=> cat.name === fact.category).color}}>{fact.category}</span>
               <div className="vote-buttons">
-                <button>👍 {fact.votesInteresting}</button>
-                <button>🤯 {fact.votesMindblowing}</button>
-                <button>⛔️ {fact.votesFalse}</button>
+                <button>👍 {fact.voteinteresting}</button>
+                <button>🤯 {fact.votemindblowing}</button>
+                <button>⛔️ {fact.votefalse}</button>
               </div>
           </li>
 }
